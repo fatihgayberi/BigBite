ac228afcbb9277d1d627791f78bf42ada2301377
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-07-26 23:29:30 +0300

    Scene

---
+++ BigBite/Assets/Script/ScrollCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraPosition();
+    }
+
+    void CameraPosition()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, GameObject.FindGameObjectWithTag("Player").transform.position.z);
+    }
+}
--- BigBite/Assets/Script/Shark.cs
+++ BigBite/Assets/Script/Shark.cs
@@ -4,56 +4,74 @@ using UnityEngine;
 
 public class Shark : MonoBehaviour
 {
-    float health = 100;
-    float mana = 0;
-    float speed = 5f;
-    float screenWidth;
-    Vector3 touchPosition;
+    float health;
+    float mana;
+    public float speed;
     float speedModifier;
-    bool moveOn = false;
+    float seaPositionZ;
+    float seconds;
+    float endedGameTimer;
+
+    public GameObject sea;
+    public GameObject finish;
+
+    bool moveOn;
+    bool gameFinish;
 
     // Start is called before the first frame update
     void Start()
     {
-        screenWidth = Screen.width;
-        Debug.Log("width: " + screenWidth);
-
-        speedModifier = 0.01f;
+        health = 100;
+        mana = 0;
+        speed = 5f;
+        speedModifier = 0.005f;
+        seaPositionZ = 23;
+        seconds = 0;
+        endedGameTimer = 5f;
+        moveOn = false;
+        gameFinish = false;
     }
-    void Update()
+
+    void FixedUpdate()
     {
         MoveSpeed();
         Move();
     }
 
-    float getHealth()
+    private void Update()
+    {
+        GameFinish();
+        SecondCounter();
+    }
+
+    public float getHealth()
     {
         return health;
     }
-    float setHealth(float health)
+    public float setHealth(float health)
     {
         return this.health = health;
          
     }
-
-    float getMana()
+    public float getMana()
     {
         return mana;
     }
-    float setMana(float mana)
+    public float setMana(float mana)
     {
         return this.mana = mana;
     }
-
-    float getSpeed()
+    public float getSpeed()
     {
         return speed;
     }
-    float setSpeed(float speed)
+    public float setSpeed(float speed)
     {
         return this.speed = speed;
     }
 
+
+    // sag sol yapmasini saglar
     void Move()
     {
         if (Input.touchCount > 0)
@@ -69,11 +87,54 @@ public class Shark : MonoBehaviour
         }
     }
 
+    // surekli olarak z ekseninde ilerlemesini saglar
     void MoveSpeed()
     {
         if (moveOn)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Back") && seconds <= endedGameTimer)
+        {
+            GameObject.Instantiate(sea, new Vector3(0, 0, seaPositionZ), Quaternion.identity);
+            seaPositionZ += 10;
+        }
+    }    
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Edge"))
+        {
+            speed = 1;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Edge"))
+        {
+            speed = 5;
+        }
+    }
+
+    void SecondCounter()
+    {
+        if (moveOn)
+        {
+            seconds += Time.deltaTime;
+        }
+    }
+
+    void GameFinish()
+    {
+        if (seconds > endedGameTimer && !gameFinish)
+        {
+            GameObject.Instantiate(finish, new Vector3(0, 0, seaPositionZ), Quaternion.identity);
+            gameFinish = true;
         }
     }
 }
