37523df9519c2aba5c3aaa9ef8bcd0cee210a3a6
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-08-04 03:45:43 +0300

    UI

--- BigBite/Assets/Script/PlayUI.cs
+++ BigBite/Assets/Script/PlayUI.cs
@@ -9,12 +9,6 @@ public class PlayUI : MonoBehaviour
     public Text fishCounter;
     SharkSwim sharkSwim;
     SharkCreate SharkCreate;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
