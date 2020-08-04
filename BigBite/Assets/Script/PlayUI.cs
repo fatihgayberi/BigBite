15b03ddbfea09ef275751226608e3a34ab796aac
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-08-04 03:27:55 +0300

    UI

---
+++ BigBite/Assets/Script/PlayUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayUI : MonoBehaviour
+{
+    public Text coinCounter;
+    public Text fishCounter;
+    SharkSwim sharkSwim;
+    SharkCreate SharkCreate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OutputCoinAndFish();
+    }
+
+    void OutputCoinAndFish()
+    {
+        sharkSwim = FindObjectOfType<SharkSwim>();
+        SharkCreate = FindObjectOfType<SharkCreate>();
+
+        fishCounter.text = sharkSwim.getFishCounter() + " BALIK";
+        coinCounter.text = SharkCreate.getCoinCounter() + " ALTIN";
+    }
+}
